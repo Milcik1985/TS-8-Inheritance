830314d01451de621d0044402f1bae05879cc827
Milcik1985 <johndoe@example.commilcik@gmail.com>
2024-03-01 10:29:37 +0200

    TS-8 Inheritance

---
+++ src/script.ts
@@ -0,0 +1,7 @@
+// 1. Sukurti klasę Figure. Klasė turi turėt parameturs "color", "isDigital". Metodą "draw";
+// 2. Sukurtos klasės parametrai turi būt protected tipo;
+// 3. Sukurkit klases Triangle ir Circle;
+// 4.  Circle iš viso turi turėt parameturs "color", "isDigital", parametrą "draw";
+// 5. Triangle iš viso turi turėt parameturs "color", "isDigital", "aLength", "bLength", "cLength", bei parametrus "draw", "getArea"; (Triagle turės turėti savo kenstruktoriu bei kviesti super() );
+// 6. Triangle sukurti "color" getterį bei setterį;
+// 7. Išanalizuot CAO 9 temos pvz;
